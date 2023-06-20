fba4e44af0bd618208dc158400406a29d0811883
dcvieira <dcvieira@tjsp.jus.br>
2023-06-20 18:01:17 -0300

    create GlobalFeedModeule

---
+++ src/app/globalFeed/components/globalFeed/globalFeed.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-feed',
+  templateUrl: './globalFeed.component.html',
+  standalone: true,
+})
+export class GlobalFeedComponent {}
